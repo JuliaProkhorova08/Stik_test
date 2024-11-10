9411dccf742711e126bc9f7c4f787c0e4d1a5377
JuliaProkhorova08 <julia.pro.smm@gmail.com>
2024-11-10 23:39:55 +0300

    add project

---
+++ components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <picture class="card__photo">
+      <source
+        media="(min-width: 1440px)"
+        srcset="
+          ~assets/img/card-desktop-01@1x.jpg 1x,
+          ~assets/img/card-desktop-01@2x.jpg 2x
+        "
+        sizes="518px"
+      />
+      <img
+        src="~assets/img/card-mobile-01@1x.jpg"
+        width="375"
+        height="617"
+        alt="Фото девушки в белом жакете."
+      />
+    </picture>
+  </div>
+</template>
+
+<style>
+.card__photo {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+.card__photo img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Header />
+    <Card />
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+html {
+  box-sizing: border-box;
+  height: 100%;
+}
+
+body {
+  display: flex;
+  flex-direction: column;
+  min-width: 375px;
+  min-height: 100%;
+  margin: 0 auto;
+  background-color: #fff;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+
+  object-fit: scale-down;
+}
+</style>
